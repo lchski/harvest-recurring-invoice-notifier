f9be47b3da09f37d842f4df3b4fcd0d5bf41a7fb
Lucas Cherkewski <lucas@ecustom.ca>
2016-01-24 10:44:46 -0500

    remove test emailer to stop spamming myself

--- src/routes.php
+++ src/routes.php
@@ -2,20 +2,6 @@
 // Routes
 
 $app->get('/', function (\Slim\Http\Request $request, \Slim\Http\Response $response, $args) {
-    $this->mailer
-        ->setTo('lucas.cherkewski@gmail.com', 'Your Email')
-        ->setSubject('Test Message 2')
-        ->setFrom('no-reply@domain.com', 'Domain.com')
-        ->addMailHeader('Reply-To', 'no-reply@domain.com', 'Domain.com')
-        ->addMailHeader('Cc', 'bill@example.com', 'Bill Gates')
-        ->addMailHeader('Bcc', 'steve@example.com', 'Steve Jobs')
-        ->addGenericHeader('X-Mailer', 'PHP/' . phpversion())
-        ->addGenericHeader('Content-Type', 'text/html; charset="utf-8"')
-        ->setMessage('<strong>This is a different test message.</strong>')
-        ->setWrap(100);
-    $send = $this->mailer->send();
-    echo ($send) ? 'Email sent successfully' : 'Could not send email';
-
     // Render response
     return $response->write("Hello!");
 });
