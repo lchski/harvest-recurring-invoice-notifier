c6b2ac420ed7c93d20c7435336f8470b35f4d9aa
Lucas Cherkewski <lucas@ecustom.ca>
2016-01-24 10:20:40 -0500

    remove a bunch of crud

--- src/routes.php
+++ src/routes.php
@@ -1,10 +1,7 @@
 <?php
 // Routes
 
-$app->get('/[{name}]', function ($request, $response, $args) {
-    // Sample log message
-    $this->logger->info("Slim-Skeleton '/' route");
-
-    // Render index view
-    return $this->renderer->render($response, 'index.phtml', $args);
+$app->get('/', function (\Slim\Http\Request $request, \Slim\Http\Response $response, $args) {
+    // Render response
+    return $response->write("Hello!");
 });
